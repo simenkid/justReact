298288b6f8918c82c9173b89bb1420f7bceef0f8
Simen Li <simenkid@gmail.com>
2016-06-04 16:43:28 +0800

    material-ui ok. should use import, not var ... require, or it won't work

---
+++ app/MyAwesomeReactComponent.js
@@ -0,0 +1,14 @@
+// var React = require('react');
+// var RaisedButton = require('material-ui/RaisedButton');
+
+
+import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+
+var MyAwesomeReactComponent = function () {
+    return (
+        <RaisedButton label="Default" />
+    );
+};
+ 
+module.exports = MyAwesomeReactComponent;
--- app/index.js
+++ app/index.js
@@ -1,14 +1,55 @@
-var React = require('react'),
-    ReactDOM = require('react-dom');
+// var React = require('react'),
+//     ReactDOM = require('react-dom');
+// import getMuiTheme from 'material-ui/styles/getMuiTheme';
+// // var getMuiTheme = require('material-ui/styles/getMuiTheme');
+// var MuiThemeProvider = require('material-ui/styles/MuiThemeProvider');
+// var MyAwesomeReactComponent = require('./MyAwesomeReactComponent');
 
-var component = require('./component');
-var HelloReact = require('./components/HelloText.js');
+// var component = require('./component');
+// var HelloReact = require('./components/HelloText.js');
 
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import MyAwesomeReactComponent from './MyAwesomeReactComponent';
 
 var element = document.createElement('div');
 element.id = "container";
 
-document.body.appendChild(component());
+// document.body.appendChild(component());
 document.body.appendChild(element);
 
-ReactDOM.render(<HelloReact />, document.getElementById('container'));
+// ReactDOM.render(<HelloReact />, document.getElementById('container'));
+
+// var App = React.createClass({
+//     render: function () {
+//         return (
+//             <div>Yo </div>
+//         );
+//     }
+// });
+
+// var App = React.createClass({
+//     render: function () {
+//         return (
+//             <MuiThemeProvider muiTheme={getMuiTheme()}>
+//                 <MyAwesomeReactComponent />
+//             </MuiThemeProvider>
+//         );
+//     }
+// });
+
+var App = function () {
+    return (
+        <MuiThemeProvider muiTheme={getMuiTheme()}>
+            <MyAwesomeReactComponent />
+        </MuiThemeProvider>
+    );
+};
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('container')
+);
