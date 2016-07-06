d631634459ddb1dd84a8682b6c582044b650858c
Simen Li <simenkid@gmail.com>
2016-07-07 01:21:17 +0800

    input text ok, need trans to notes

--- app/components/NoteInput.js
+++ app/components/NoteInput.js
@@ -2,14 +2,31 @@ import React from 'react';
 import TextField from 'material-ui/TextField';
 
 var NoteInput = React.createClass({
+    getInitialState: function () {
+        return { value: '' }
+    },
+    onChanged: function (evt) {
+        var inputStr = evt.target.value;
+        var pattern = /[0-4,\s\n\r\t]$/;
+        var newState = { value: null };
+
+        if (pattern.test(inputStr)) {  // returns a boolean
+            newState.value = inputStr.slice(0, inputStr.length);
+            this.setState(newState);
+
+            if (this.props.onChanged)
+                this.props.onChanged(newState.value);
+        }
+    },
     render: function () {
         return (
-            <TextField
-              style={this.props.style}
+            <TextField {...this.props}
               hintText="01012103"
               floatingLabelText="Insert notes here:"
               multiLine={true}
-              rows={4}
+              rows={2}
+              onChange={this.onChanged}
+              value={this.state.value}
             />
         );
     }
--- app/components/Panel.js
+++ app/components/Panel.js
@@ -13,13 +13,25 @@ const style = {
 };
 
 var Panel = React.createClass({
+    getInitialState: function () {
+      return {
+        notesText: null
+      }
+    },
+    onChanged: function (text) {
+      this.setState({
+        notesText: text
+      });
+    },
     render: function () {
         return (
           <div style={this.props.style}>
             <SpeedSelector style={{marginBottom: "20px"}} />
             <Divider />
-              <Player style={{marginLeft: "20px", marginTop: "20px"}}/>
-              <NoteInput style={{marginLeft: "20px", marginTop: "0px"}}/>
+            <Player style={{marginLeft: "20px", marginRight: "20px", marginTop: "20px"}} notes={this.state.notesText} />
+            <div style={{marginRight: "20px"}}>
+              <NoteInput fullWidth={true} style={{marginLeft: "20px", marginTop: "0px"}} onChanged={this.onChanged} />
+            </div>
           </div>
         );
     }
